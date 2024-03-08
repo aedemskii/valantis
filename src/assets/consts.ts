fb963116d9db94c7d15e0385383899c71edbd4db
Alexander Edemskii <a_edemskii@EdemMBP.local>
2024-03-08 20:29:19 +0300

    requests retry in case of server error

--- src/assets/consts.ts
+++ src/assets/consts.ts
@@ -1,3 +1,5 @@
 export const ITEMS_PER_PAGE = 50;
 
+export const TRIES_PER_FETCH = 2;
+
 export const API_URL = 'http://api.valantis.store:40000/';
--- src/hooks/useProductList.ts
+++ src/hooks/useProductList.ts
@@ -1,9 +1,10 @@
 import { useState, useEffect } from "react";
-import { ITEMS_PER_PAGE } from "../assets/consts";
+import { ITEMS_PER_PAGE, TRIES_PER_FETCH } from "../assets/consts";
 import {
   TItem,
   TItemsFilterParams,
-  TProductListResult
+  TProductListResult,
+  TItemsFilterParamsForFetch
 } from "../assets/types";
 import {
   requestDefaultItemsIds,
@@ -27,20 +28,23 @@ export const useProductList = (filterParams: TItemsFilterParams): TProductListRe
     setLoading(true);
     setError(null);
 
-    if (brands.length === 0) {
-      (async () => {
-        try {
-          const response = await fetchFields(abortController.signal);
-          setBrands([...new Set(response.result)].filter(brand => brand));
-        } catch (error: unknown) {
-          if (error instanceof Error) {
-            console.error(error.message);
+    const fetchBrands = async (retry?: number) => {
+      try {
+        const response = await fetchFields(abortController.signal);
+        setBrands([...new Set(response.result)].filter(brand => brand));
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error(error.message);
+          if (retry && retry > 0) {
+            fetchBrands(retry - 1);
+          } else {
             setError(error.message);
           }
         }
-      })();
-    }
-    const requestItemsByIds = async (itemsIds: string[]) => {
+      }
+    };
+
+    const requestItemsByIds = async (itemsIds: string[], retry?: number) => {
       try {
         const response = await fetchItemsByIds([...itemsIds]);
         const uniqueProducts: Map<string, TItem> = new Map();
@@ -52,81 +56,101 @@ export const useProductList = (filterParams: TItemsFilterParams): TProductListRe
       } catch (error: unknown) {
         if (error instanceof Error) {
           console.error(error.message);
-          setError(error.message);
+          if (retry && retry > 0) {
+            requestItemsByIds(itemsIds, retry - 1);
+          } else {
+            setError(error.message);
+          }
         }
       } finally {
         setLoading(false);
       }
     };
 
+    const getFilteredItems = async (filterParamsForFetch: TItemsFilterParamsForFetch, retry?: number) => {
+      try {
+        const response = await requestFilteredItemsIds(
+          filterParamsForFetch,
+          abortController.signal
+        );
+        const itemsIds = [...new Set(response.result)];
+        setFilteredItemsIds(itemsIds);
+        requestItemsByIds(itemsIds.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE), TRIES_PER_FETCH);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error(error.message);
+          if (retry && retry > 0) {
+            getFilteredItems(filterParamsForFetch, retry - 1);
+          } else {
+            setError(error.message);
+          }
+        }
+      }
+    }
+
+    const getUnfilteredItemsIds = async (retry?: number) => {
+      try {
+        const tempItemIds: Set<string> = new Set();
+        let offset = (page - 1) * ITEMS_PER_PAGE;
+        while (tempItemIds.size < ITEMS_PER_PAGE) {
+          const response = await requestDefaultItemsIds(
+            offset,
+            ITEMS_PER_PAGE - tempItemIds.size,
+            abortController.signal
+          );
+          const ids = response.result;
+          ids.forEach((id: string) => {
+            tempItemIds.add(id);
+          });
+          offset += ids.length;
+        }
+        setUnfilteredItemsIds(prev => {
+          const arr = [...tempItemIds];
+          for (let i = (page - 1) * ITEMS_PER_PAGE; i < page * ITEMS_PER_PAGE; i++) {
+            prev[i] = arr[i - (page - 1) * ITEMS_PER_PAGE];
+          }
+          return [...new Set(prev)];
+        });
+        requestItemsByIds([...tempItemIds], TRIES_PER_FETCH);
+      } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.error(error.message);
+          if (retry && retry > 0) {
+            getUnfilteredItemsIds(retry - 1);
+          } else {
+            setError(error.message);
+          }
+        }
+      }
+      };
+
+    if (brands.length === 0) {
+      fetchBrands(TRIES_PER_FETCH);
+    }
+
     const filterParamsForFetch = parseParamsForFetch(filterParams);
     const page = filterParams.page;
     if (filterParamsForFetch === null) {
       const ids = unfilteredItemsIds.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE);
       if (!hasNullValues(ids) && ids.length === ITEMS_PER_PAGE) {
-        requestItemsByIds(ids);
+        requestItemsByIds(ids, TRIES_PER_FETCH);
       } else {
-        (async () => {
-          try {
-            const tempItemIds: Set<string> = new Set();
-            let offset = (page - 1) * ITEMS_PER_PAGE;
-            while (tempItemIds.size < ITEMS_PER_PAGE) {
-              const response = await requestDefaultItemsIds(
-                offset,
-                ITEMS_PER_PAGE - tempItemIds.size,
-                abortController.signal
-              );
-              const ids = response.result;
-              ids.forEach((id: string) => {
-                tempItemIds.add(id);
-              });
-              offset += ids.length;
-            }
-            setUnfilteredItemsIds(prev => {
-              const arr = [...tempItemIds];
-              for (let i = (page - 1) * ITEMS_PER_PAGE; i < page * ITEMS_PER_PAGE; i++) {
-                prev[i] = arr[i - (page - 1) * ITEMS_PER_PAGE];
-              }
-              return [...new Set(prev)];
-            });
-            requestItemsByIds([...tempItemIds]);
-          } catch (error: unknown) {
-            if (error instanceof Error) {
-              console.error(error.message);
-              setError(error.message);
-            }
-          }
-        })();
+        getUnfilteredItemsIds(TRIES_PER_FETCH);
       }
     } else {
       const filterParamsStr = JSON.stringify(parseParamsForFetch(filterParams));
       if (filterParamsStr === lastFilterParamsStr && filteredItemsIds.length > 0) {
-        requestItemsByIds(filteredItemsIds.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE));
+        requestItemsByIds(filteredItemsIds.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE), TRIES_PER_FETCH);
       } else {
         setLastFilterParamsStr(filterParamsStr);
-        (async () => {
-          try {
-            const response = await requestFilteredItemsIds(
-              filterParamsForFetch,
-              abortController.signal
-            );
-            const itemsIds = [...new Set(response.result)];
-            setFilteredItemsIds(itemsIds);
-            requestItemsByIds(itemsIds.slice((page - 1) * ITEMS_PER_PAGE, page * ITEMS_PER_PAGE));
-          } catch (error: unknown) {
-            if (error instanceof Error) {
-              console.error(error.message);
-              setError(error.message);
-            }
-          }
-        })();
+        getFilteredItems(filterParamsForFetch, TRIES_PER_FETCH);
       }
     }
 
     return () => {
       abortController.abort();
     };
-  }, [filterParams, error]);
+  }, [filterParams]);
 
   return {
     items,
